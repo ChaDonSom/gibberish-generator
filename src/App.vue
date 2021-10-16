93a5db10c53efb8bfe0867d52cf77e44a48ce6ab
Chase Penguin Plus v2 <chadonsom@gmail.com>
2021-10-15 21:42:35 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Generator</h1>
+  <button @click="generate" class="bg-green-700 text-white p-2 px-3 rounded-full">Generate</button>
+  <p id="blurb" class="grid grid-cols-5 grid-flow-col auto-cols-max">
+    <span v-for="word of words" :key="word.word">{{ word.word }}&nbsp;</span>
+  </p>
+  <p id="info" class="grid grid-cols-5 grid-flow-col auto-cols-max">
+    <span v-for="word of words" :key="word.word"
+      >{{ word.partOfSpeech }}&nbsp;</span
+    >
+  </p>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<script setup>
+import { ref } from "@vue/reactivity";
+import axios from "axios";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+const words = ref([]);
+
+async function generate() {
+  const total = 5;
+
+  let responses = [];
+  let partsOfSpeech = [
+    "preposition",
+    "conjunction",
+    "noun",
+    "pronoun",
+    "verb",
+    "adverb",
+    "adjective",
+  ];
+  for (let i = 0; i < total; i++) {
+    responses.push(
+      axios.request({
+        method: "GET",
+        url: "https://wordsapiv1.p.rapidapi.com/words/",
+        headers: {
+          "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "c79abe840bmsh5c471bf03d34f87p1c21a5jsn0ebeca62307f",
+        },
+        params: {
+          random: true,
+          partOfSpeech:
+            partsOfSpeech[Math.floor(Math.random() * partsOfSpeech.length)],
+          hasDetails: "definitions",
+        },
+      })
+    );
+  }
+
+  responses = await Promise.all(responses);
+
+  console.log(responses);
+
+  let wordOrder = {
+    pronoun: 2,
+    adjective: 3,
+    adverb: 4,
+    verb: 5,
+    noun: 6,
+  };
+  let sorted = responses
+    .sort(() => Math.random() - 0.5)
+    .map(({ data }) => data)
+    .sort((a, b) => {
+      let partA = wordOrder[a.results?.[0]?.partOfSpeech];
+      let partB = wordOrder[b.results?.[0]?.partOfSpeech];
+      if (partA === undefined) return Math.random() - 0.5;
+      if (partB === undefined) return Math.random() - 0.5;
+      if (partA > partB) return 1;
+      if (partB > partA) return -1;
+      return 0;
+    });
+
+  words.value = sorted.map((data) => ({
+    word: data.word,
+    partOfSpeech: data.results[0]?.partOfSpeech,
+  }));
+}
 </script>
 
 <style>
